d7b024c86750bd5691efda69bfbae2ba6b11c6a3
BulatSalakhutdinov <vaiiio@yandex.ru>
2015-01-11 21:49:50 +0300

    Finish user signup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.new(post_params)
 		if @post.save
+			flash[:success] = "Post was successfully created"
 			redirect_to @post
 		else
 			render 'new'
